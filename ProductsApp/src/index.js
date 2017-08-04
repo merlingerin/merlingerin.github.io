599404a61018465599ce02c2eaab5322b1033f1d
merlingerin <lisg1990@gmail.com>
2017-08-04 17:27:30 +0300

    clearFolder

--- ProductsApp/src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-// import App from './App';
-import AppContainer from './AppContainer';
-import registerServiceWorker from './registerServiceWorker';
-
-ReactDOM.render(<AppContainer />, document.getElementById('root'));
-registerServiceWorker();
